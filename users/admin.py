328680bdeb4f04a47a86808dbd8e22d992d0c3f6
dmitriy.balashov <reaver_std@mail.ru>
2024-01-18 10:59:42 +0500

    adminpanel for user refactor

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+class UserAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff')})
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2', 'is_active', 'is_staff')
+        }),
+    )
+    list_display = ('email', 'is_active', 'is_staff')
+
+
+admin.site.register(User, UserAdmin)
