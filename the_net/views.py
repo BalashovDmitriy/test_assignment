e7b6a84af66e8e0e638b7570d9e8acad9e1d6da0
dmitriy.balashov <reaver_std@mail.ru>
2024-01-20 20:23:03 +0500

    add django_filters

--- the_net/views.py
+++ the_net/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
 
+from the_net.filters import SellerFilter
 from the_net.models import Seller
 from the_net.serializers import SupplierSerializer
 
@@ -9,4 +10,5 @@ class SupplierViewSet(viewsets.ModelViewSet):
     queryset = Seller.objects.all()
     serializer_class = SupplierSerializer
     permission_classes = (permissions.IsAuthenticated,)
-
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = SellerFilter
